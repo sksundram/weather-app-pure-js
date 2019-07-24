1c9eae3513e4869f85f0cc36af184669207967de
Shailesh Kumar Sundram <shailesh.sundram@gmail.com>
2019-07-24 15:11:01 +0530

    Find weather data

---
+++ scripts/forecast.js
@@ -0,0 +1,28 @@
+// AccuWeather api key
+const API_KEY = 'R92O9d8pB14vhXzBCE6JArRm4HAmcVfP';
+
+// Get weather information
+const getWeather = async locationKey => {
+  const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+  const query = `${locationKey}?apikey=${API_KEY}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data[0];
+};
+
+// Get city information
+const getCity = async city => {
+  const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+  const query = `?apikey=${API_KEY}&q=${city}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+  return data[0];
+};
+
+getCity('bengaluru')
+  .then(cityData => getWeather(cityData.Key))
+  .then(weatherData => console.log(weatherData))
+  .catch(err => console.log(err));
